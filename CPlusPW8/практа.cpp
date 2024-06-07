3e63c483cbd564ff2c2cfd530f09f2893b33d579
maksv1 <isip_m.v.vinogorov@mpt.ru>
2024-06-07 14:19:39 +0300

    Практос 8 C++

---
+++ CPlusPW8/практа.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+typedef bool (*ContainsAllCharsFunc)(const char*, const char*);
+
+int main() {
+    setlocale(0, "rus");
+    string inputStr;
+    string inputChars;
+
+    cout << "Введите строку: ";
+    std::cin >> inputStr;
+
+    cout << "Введите символы для поиска: ";
+    std::cin >> inputChars;
+
+    HINSTANCE hLib = LoadLibrary(L"DynamicLib.dll");
+    if (hLib == NULL) {
+        cerr << "Не удалось загрузить библиотеку!" << endl;
+        return 1;
+    }
+
+    ContainsAllCharsFunc contains_all_chars = (ContainsAllCharsFunc)GetProcAddress(hLib, "contains_all_chars");
+    if (contains_all_chars == NULL) {
+        cerr << "Не удалось получить адрес функции!" << endl;
+        FreeLibrary(hLib);
+        return 1;
+    }
+
+    bool result = contains_all_chars(inputStr.c_str(), inputChars.c_str());
+
+    if (result) {
+        cout << "Содержит ли строка \"" << inputStr << "\" символы \"" << inputChars << "\" ?\n";
+        cout << "Да." << endl;
+    }
+    else {
+        cout << "Содержит ли строка \"" << inputStr << "\" символы \"" << inputChars << "\" ?\n";
+        cout << "Нет." << endl;
+
+    }
+
+    FreeLibrary(hLib);
+
+    return 0;
+}
